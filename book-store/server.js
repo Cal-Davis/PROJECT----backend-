a8ed69822c416290cce5d54009ad1a6b0ee961e2
Caldavis Irurah <caldaviswangaruro@gmail.com>
2023-07-29 14:10:38 +0300

    updated

--- book-store/server.js
+++ book-store/server.js
@@ -4,13 +4,13 @@ const PORT = process.env.PORT || 4040
 const express = require ('express')
 const mongoose = require ('mongoose')
 const router = require('./routes/Bookroutes')
-const corsOptions = require('./config/corsOptions') 
+
 const cors = require('cors')
 const app  = express()
 //MIDDLEWARE
 
 
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json())
 app.use('/books' , router)
 
